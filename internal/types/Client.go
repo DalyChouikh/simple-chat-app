c12ad8e2c4692bb9f712bbd48bf25e1dafb1b218
DalyChouikh <chouikhdaly215@gmail.com>
2024-11-17 22:36:36 +0100

    Added Read and Write functions to the Client type and exposed its ID field

--- internal/types/Client.go
+++ internal/types/Client.go
@@ -1,17 +1,53 @@
 package types
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
-	id   string
+	ID   string
 	conn *websocket.Conn
 	send chan []byte
 }
 
 func NewClient(id string, conn *websocket.Conn) *Client {
 	return &Client{
-		id:   id,
+		ID:   id,
 		conn: conn,
 		send: make(chan []byte),
+	}
+}
+
+func (c *Client) Read(manager *ClientManager) {
+	defer func() {
+		manager.Unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			manager.Unregister <- c
+			c.conn.Close()
+			break
+		}
+		jsonMessage, _ := json.Marshal(NewMessage(c.ID, "", string(message)))
+		manager.Broadcast <- jsonMessage
+	}
+}
+
+func (c *Client) Write() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+			}
+			c.conn.WriteMessage(websocket.TextMessage, message)
+		}
 	}
 }
